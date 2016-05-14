97f5c027d484be5d922423a58d81323a60ec9664
Alex Hanna <tinnvec@gmail.com>
2016-05-14 17:35:45 -0500

    Institute config file

--- stonebot.rb
+++ stonebot.rb
@@ -2,18 +2,22 @@
 # https://discordapp.com/oauth2/authorize?client_id=181041901225377793&scope=bot&permissions=19456
 
 require 'discordrb'
+require 'yaml'
 
 require_relative 'lib/services/hearthstone_api'
 
+config = YAML.load(File.open 'config.yml')
+
 bot = Discordrb::Bot.new(
-	token: 'MTgxMDQyMTY0NDkzNDUxMjY0.ChjAJw.TBLiJ1UuLLxJ6-O5rjI1fGrBFkI',
-	application_id: 181042164493451264)
+	token:			config[:discord][:token],
+	application_id:	config[:discord][:application_id])
 
-hearthstone_api = Services::HearthstoneApi.new('DwEDGfQK1Kmshu4zl0nRQ73WYf0Vp1uPb04jsnZ4JeqrI8Kdaf')
+hearthstone_api = Services::HearthstoneApi.new(config[:hearthstone_api][:mashape_key])
 
-bot.message(contains: /.*\[(.*)\].*/) do |event|
-	cardname = /.*\[(.*)\].*/.match(event.message.content)[1]
-	event.respond hearthstone_api.find_gold_card(cardname)
+hs_card_regex = Regexp.new('.*\[(.*)\].*')
+bot.message(contains: hs_card_regex) do |event|
+	card_name = hs_card_regex.match(event.message.content)[1]
+	event.respond hearthstone_api.find_gold_card(card_name)
 end
 
 bot.run
