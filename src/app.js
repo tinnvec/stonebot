555f842bb509f8f4b81c29b46ce63acf24922fcd
Alex Hanna <tinnvec@gmail.com>
2016-12-02 14:48:38 -0600

    Add jsdom-global dependency

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
+import 'jsdom-global/register'
+
 import Discord from 'discord.js'
 import HearthstoneJSON from 'hearthstonejson'
 
 import config from './config/config'
 
 const client = new Discord.Client()
-const hsjson = new HearthstoneJSON();
+const hsjson = new HearthstoneJSON()
 
 client.on('ready', () => {
     console.log('Client ready')
