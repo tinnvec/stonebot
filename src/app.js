c43893bdbca004fe4f55a9bbe82bef846b36295d
Alex Hanna <tinnvec@gmail.com>
2016-12-02 12:00:05 -0600

    Add hearthstonejson initialization

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Discord from 'discord.js'
+import HearthstoneJSON from 'hearthstonejson'
 
 import config from './config/config'
 
 const client = new Discord.Client()
+const hsjson = new HearthstoneJSON();
 
 client.on('ready', () => {
     console.log('Client ready')
