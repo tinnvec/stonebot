c195c718f3a7eae9f07b8f14f004c5f48b91b6a2
Alex Hanna <tinnvec@gmail.com>
2016-12-25 14:25:45 -0600

    Update art size from 256px to 512px

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ function formatOutput(card, addon) {
   }
 
   if (addon === 'art') {
-    result += `\nhttps://art.hearthstonejson.com/v1/256x/${card.id}.jpg`
+    result += `\nhttps://art.hearthstonejson.com/v1/512x/${card.id}.jpg`
   }
 
   return result
