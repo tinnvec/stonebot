7a33bca9323938c421d7cebfb4480305e339d882
Alex Hanna <tinnvec@gmail.com>
2017-01-07 10:46:15 -0600

    Refactor card methods to helper file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import Discord from 'discord.js'
-import Fuse from 'fuse.js'
-import HearthstoneJSON from 'hearthstonejson'
-import toMarkdown from 'to-markdown'
 import ffmpeg from 'fluent-ffmpeg'
 
 import messageHandler from './handlers/message-handler'
 import config from './config/config'
 
 const client = new Discord.Client()
-const hsjson = new HearthstoneJSON()
 
 if (config.logLevel > 1) { client.on('debug', console.info) }
 if (config.logLevel > 0) { client.on('warn', console.warn) }
@@ -82,87 +78,4 @@ function getMessageAuthorVoiceChannel(message) {
         }
     })
     return result
-}
-
-function detectCardMentions(message) {
-    let pattern = /:{2}([^:\?]+)\??([^:]*):{2}/g
-    let matches = []
-    let match = pattern.exec(message.content)
-    while (match) {
-        matches.push(match)
-        match = pattern.exec(message.content)
-    }
-    return matches
-}
-
-function searchForCard(allCards, pattern) {
-    let uncollectibleOnly = false
-    if (pattern.startsWith('@')) {
-        pattern = pattern.substring(1)
-        uncollectibleOnly = true
-    }
-
-    let uncollectibleFuse = new Fuse(
-        allCards.filter(card => { return !card.collectible }),
-        { keys: ['name'], include: ['score'] }
-    )
-    let foundUncollectible = uncollectibleFuse.search(pattern)
-
-    if (uncollectibleOnly) {
-        return foundUncollectible[0].item
-    }
-
-    let collectibleFuse = new Fuse(
-        allCards.filter(card => { return card.collectible }),
-        { keys: ['name'], include: ['score'] }
-    )
-    let foundCollectible = collectibleFuse.search(pattern)
-
-    if (foundUncollectible[0].score < foundCollectible[0].score) {
-        return foundUncollectible[0].item
-    }
-    return foundCollectible[0].item
-}
-
-function formatOutput(card, addon) {
-    if (addon === 'image') {
-        return `http://media.services.zam.com/v1/media/byName/hs/cards/enus/${card.id}.png`
-    }
-
-    if (addon === 'gold') {
-        return `http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/${card.id}_premium.gif`
-    }
-
-    if (addon === 'art') {
-        return `https://art.hearthstonejson.com/v1/512x/${card.id}.jpg`
-    }
-
-    let result = `${card.name} -`
-
-    if (card.cost) {
-        result += ` ${card.cost} Mana`
-    }
-
-    if (card.attack) {
-        result += ` ${card.attack}/${card.health || card.durability}`
-    }
-
-    result += ` ${card.playerClass.toLowerCase().capitalizeFirstLetter()}`
-    result += ` ${card.type.toLowerCase().capitalizeFirstLetter()}`
-
-    if (card.collectionText) {
-        result += `\n${toMarkdown(card.collectionText)}`
-    } else if (card.text) {
-        result += `\n${toMarkdown(card.text)}`
-    }
-
-    if (addon === 'flavor' && card.flavor) {
-        result += `\n${card.flavor}`
-    }
-
-    return result
-}
-
-String.prototype.capitalizeFirstLetter = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1)
 }
