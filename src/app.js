3186b25a100d7c9f2d9a74869cdd7038c80ec72c
Alex Hanna <tinnvec@gmail.com>
2017-01-07 10:39:01 -0600

    Add logging for current guilds, guild join/leave and dis/reconnect

--- src/app.js
+++ src/app.js
@@ -15,7 +15,15 @@ if (config.logLevel > 1) { client.on('debug', console.info) }
 if (config.logLevel > 0) { client.on('warn', console.warn) }
 client.on('error', console.error)
 
+client.on('guildCreate', guild => { console.log(`Joined Guild: ${guild.name}`) })
+client.on('guildDelete', guild => { console.log(`Left Guild: ${guild.name}`) })
+
+client.on('reconnecting', () => { console.log('Reconnecting...')})
+client.on('disconnect', event => { console.log(`Disconnected: ${event.reason}`)} )
+
 client.on('ready', () => {
+    let guilds = client.guilds.map(guild => { return guild.name })
+    console.log(`Current Guilds: ${guilds.join(', ')}`)
     client.user.setGame('Hearthstone')
 })
 
