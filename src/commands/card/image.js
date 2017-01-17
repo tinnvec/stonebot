025630e4512cae47aed582ead608f9111489dfc4
Alex Hanna <tinnvec@gmail.com>
2017-01-17 11:11:15 -0600

    Add card text and image commands Remove message-handler.js

---
+++ src/commands/card/image.js
@@ -0,0 +1,31 @@
+import Discord from 'discord.js'
+import { Command } from 'discord.js-commando'
+import Card from '../../card/card'
+
+module.exports = class ImageCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'image',
+            aliases: ['img', 'i', '📷', '📸'],
+            group: 'card',
+            memberName: 'image',
+            description: 'Displays card image.',
+            args: [
+                {
+                    key: 'name',
+                    prompt: 'what card are you searching for?\n',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+    async run(msg, args) {
+        if (!msg.channel.typing) { msg.channel.startTyping() }
+        const card = await Card.findByName(args.name).catch(console.error)
+        const embed = new Discord.RichEmbed()
+            .setImage(card.getImageUrl())
+        if (msg.channel.typing) { msg.channel.stopTyping() }
+        return msg.embed(embed)
+    }
+}
