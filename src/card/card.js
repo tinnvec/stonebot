c1b1818283aef36ccf3d78725242a80f89e4f069
Alex Hanna <tinnvec@gmail.com>
2017-02-06 15:31:12 -0600

    Convert colors to integers

--- src/card/card.js
+++ src/card/card.js
@@ -9,17 +9,17 @@ import winston from 'winston'
 
 import cardSoundsById from './card-sounds-by-id'
 
-const CLASS_COLORS = { // RGB
-    'WARRIOR': [196, 30, 59],
-    'SHAMAN': [0, 122, 222],
-    'ROGUE': [0, 0, 0],
-    'PALADIN': [255, 245, 105],
-    'HUNTER': [171, 212, 115],
-    'DRUID': [199, 156, 110],
-    'WARLOCK': [148, 130, 201],
-    'MAGE': [105, 204, 240],
-    'PRIEST': [255, 255, 255],
-    'NEUTRAL': [128, 128, 128]
+const CLASS_COLORS = {
+    'WARRIOR': 12852795, // #C41E3B
+    'SHAMAN': 31454, // #007ADE
+    'ROGUE': 0, // #000000
+    'PALADIN': 16774505, // #FFF569
+    'HUNTER': 11261043, // #ABD473
+    'DRUID': 13081710, // #C79C6E
+    'WARLOCK': 9732809, // #9482C9
+    'MAGE': 6933744, // #69CCF0
+    'PRIEST': 16777215, // #FFFFFF
+    'NEUTRAL': 8421504 //#808080
 }
 
 export default class Card {
