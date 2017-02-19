459c84c0ed1c03aa448fafbf3be2abe51724ee6c
Alex Hanna <tinnvec@gmail.com>
2017-02-19 10:12:35 -0600

    Code formatting

--- src/community/quest.js
+++ src/community/quest.js
@@ -2,9 +2,13 @@ const PostgreSQL = require('../postgresql/postgresql')
 const Sequelize = require('sequelize')
 
 const postgresql = new PostgreSQL()
-const Quest = postgresql.db.define('quest', {
-    userId: { type: Sequelize.STRING },
-    bnetServer: { type: Sequelize.STRING }
-})
+
+const Quest = postgresql.db.define('quest',
+    {
+        userId: { type: Sequelize.STRING },
+        bnetServer: { type: Sequelize.STRING }
+    }
+)
 Quest.sync()
+
 module.exports = Quest
