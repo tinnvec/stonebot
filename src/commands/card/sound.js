da20d3aaf091a75dad2a6e326cd0efe6d85e7956
Alex Hanna <tinnvec@gmail.com>
2017-02-19 17:57:56 -0600

    Separate replies and check permissions

--- src/commands/card/sound.js
+++ src/commands/card/sound.js
@@ -3,8 +3,6 @@ const { Command } = require('discord.js-commando')
 
 const winston = require('winston')
 
-const SOUND_KINDS = ['play', 'attack', 'trigger', 'death']
-
 class SoundCommand extends Command {
     constructor(client) {
         super(client, {
@@ -41,7 +39,9 @@ class SoundCommand extends Command {
     }
 
     async run(msg, args) {
-        if (!SOUND_KINDS.includes(args.soundKind)) {
+        if (!msg.channel.permissionsFor(this.client.user).hasPermission('SEND_MESSAGES')) { return }
+
+        if (!['play', 'attack', 'trigger', 'death'].includes(args.soundKind)) {
             args.cardName = `${args.soundKind} ${args.cardName}`.trim()
             args.soundKind = 'play'
         }
@@ -53,18 +53,16 @@ class SoundCommand extends Command {
         if (!args.soundKind || !args.cardName) { return msg.reply('cancelled command.').catch(winston.error) }
         
         if (!msg.channel.typing) { msg.channel.startTyping() }
-        let reply, sounds
         const card = await Card.findByName(args.cardName).catch(winston.error)
-        if (!card) { reply = `sorry, I couldn't find a card with a name like '${args.cardName}'` }
-        else {
-            sounds = card.getSoundParts(args.soundKind)
-            if (!sounds || sounds.length < 1) { reply = `sorry, I don't know the ${args.soundKind} sound for ${card.name}.` }
-        }
+        if (msg.channel.typing) { msg.channel.stopTyping() }
         
-        return msg.reply(reply || `I'll join your voice channel and play the ${args.soundKind} sound for ${card.name} in a moment.`)
-            .then(m => { if (m.channel.typing) { m.channel.stopTyping() } })
+        if (!card) { return msg.reply(`sorry, I couldn't find a card with a name like '${args.cardName}'`).catch(winston.error) }
+
+        const sounds = card.getSoundParts(args.soundKind)
+        if (!sounds || sounds.length < 1) { return msg.reply(`sorry, I don't know the ${args.soundKind} sound for ${card.name}.`).catch(winston.error) }
+        
+        return msg.reply(`I'll join your voice channel and play the ${args.soundKind} sound for ${card.name} in a moment.`)
             .then(() => {
-                if (reply) { return }
                 SoundCommand.queue.push({ message: msg, card: card, soundKind: args.soundKind })
                 if (SoundCommand.queue.length === 1) { SoundCommand.playSound(this.client).catch(winston.error) }
             }).catch(winston.error)
@@ -80,19 +78,41 @@ class SoundCommand extends Command {
         const card = this.queue[0].card
         const soundKind = this.queue[0].soundKind
         
-        let reply, connection
         const file = await card.getSound(soundKind).catch(winston.error)
-        if (!file) { reply = `sorry, I wasn't able to get the ${soundKind} sound for ${card.name}.` }
-        else {
-            connection = await this.joinVoiceChannel(message.member.voiceChannel).catch(winston.error)
-            if (!connection || typeof connection === 'string') {
-                if (message.member.voiceChannel) { message.member.voiceChannel.leave() }
-                reply = `sorry, there was an error joining your voice channel, ${connection || 'unkown'}`
-            }
+        if (!file) {
+            return message.reply(`sorry, I wasn't able to get the ${soundKind} sound for ${card.name}.`)
+                .then(() => {
+                    this.queue.shift()
+                    if (this.queue.length > 0) { this.playSound(client) }
+                }).catch(winston.error)
         }
-        
-        if (reply) {
-            return message.reply(reply)
+
+        if (!message.member.voiceChannel || message.member.voiceChannel.type !== 'voice') {
+            return message.reply('unable to play sound, you\'re not in a voice channel')
+                .then(() => {
+                    this.queue.shift()
+                    if (this.queue.length > 0) { this.playSound(client) }
+                }).catch(winston.error)
+        }
+
+        if (!message.member.voiceChannel.permissionsFor(client.user).hasPermission('SPEAK')) {
+            return message.reply('sorry, I don\'t have permission to speak in your voice channel')
+                .then(() => {
+                    this.queue.shift()
+                    if (this.queue.length > 0) { this.playSound(client) }
+                }).catch(winston.error)
+        }
+
+        let connection = client.voiceConnections.find(conn => conn.channel === message.member.voiceChannel)
+        if (!connection) {
+            connection = await message.member.voiceChannel.join().catch(err => {
+                return `sorry, there was an error joining your voice channel, ${err ? err.message.replace('You', 'I') : 'unknown'}`
+            })
+        }
+
+        if (typeof connection === 'string') {
+            if (message.member.voiceChannel) { message.member.voiceChannel.leave() }
+            return message.reply(connection)
                 .then(() => {
                     this.queue.shift()
                     if (this.queue.length > 0) { this.playSound(client) }
@@ -106,13 +126,6 @@ class SoundCommand extends Command {
                 this.playSound(client)
             } else { connection.channel.leave() }
         })
-    }
-
-    async joinVoiceChannel(voiceChannel) {
-        if (!voiceChannel || voiceChannel.type !== 'voice') { return 'you\'re not in one' }
-        const connection = this.client.voiceConnections.find(conn => conn.channel === voiceChannel)
-        if (connection) { return connection }
-        return await voiceChannel.join().catch(err => { return err.message.replace('You', 'I') })
     }
 }
 
