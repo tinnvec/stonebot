8d6bb6353f95088bf4130baa1652248f0fc9ecbe
Alex Hanna <tinnvec@gmail.com>
2017-03-10 09:05:58 -0600

    build -> built

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const gulpTypescript = require('gulp-typescript')
 const project = gulpTypescript.createProject('tsconfig.json')
 
 gulp.task('build', () => {
-    del.sync(['./build/**/*.*'])
+    del.sync(['./built/**/*.*'])
 
     gulp.src('./src/**/*.ts')
         .pipe(project())
-        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('built/'))
 
     gulp.src('./src/**/*.js')
-        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('built/'))
 
     gulp.src('./src/**/*.json')
-        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('built/'))
 })
